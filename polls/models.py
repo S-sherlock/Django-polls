619261a26e31b254da84aadce7e9ac9b64bb42f1
sherlock <260641084@qq.com>
2018-12-14 11:44:37 +0800

    往数据库插入数据

--- polls/models.py
+++ polls/models.py
@@ -18,14 +18,27 @@ create table votes if not exists(
 
 """
 
-
+from datetime import datetime, timedelta
+from django.utils import timezone
 from django.db import models
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_data = models.DateTimeField('发布时间')
+    pub_date = models.DateTimeField('发布时间')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        if timezone.now() - timedelta(days=1) <= self.pub_date:
+            return True
+        else:
+            return False
+
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField('选项内容', max_length=200)
     votes = models.IntegerField('投票数', default=0)
+
